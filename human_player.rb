1ba922a461baf5a50a61792555adfa543fd46c6a
App Academy Student <student@appacademy.io>
2016-07-19 15:23:11 -0700

    updated errors

--- human_player.rb
+++ human_player.rb
@@ -4,6 +4,7 @@ class HumanPlayer < Player
 
   def make_move
     start_pos = @display.move
+    raise "Empty space!!" if @display.board.empty?(start_pos)
     raise "Wrong color!!" if @display.board[start_pos].color != @color
     end_pos = @display.move
     return [start_pos, end_pos]
